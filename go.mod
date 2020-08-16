5ac8d9b0c64fe232e29bd0d266d29a3077cfe61c
sndstudy <sanada2054tech@gmail.com>
2020-08-16 23:50:05 +0900

    clean: 微修正

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module snd/selenium
 
 go 1.14
 
-require github.com/sclevine/agouti v3.0.0+incompatible // indirect
+require github.com/sclevine/agouti v3.0.0+incompatible
--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/sclevine/agouti"
+	"github.com/sclevine/agouti"
 )
 
 func main() {
-    options := agouti.ChromeOptions(
-        "args", []string{
-            "--headless",
-            "--disable-gpu",
-            "--window-size=1280,800",
-            "--disable-dev-shm-usage",
-            "--no-sandbox",
-        })
+	options := agouti.ChromeOptions(
+		"args", []string{
+			"--headless",
+			"--disable-gpu",
+			"--window-size=1280,800",
+			"--disable-dev-shm-usage",
+			"--no-sandbox",
+		})
 
-    driver := agouti.ChromeDriver(options)
-    defer driver.Stop()
-    driver.Start()
+	driver := agouti.ChromeDriver(options)
+	defer driver.Stop()
+	driver.Start()
 
-    page, err := driver.NewPage()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "%s\n", err)
-        return
-    }
+	page, err := driver.NewPage()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
 
-    page.Navigate("https://www.google.com/")
-    page.Screenshot("./screenshot.jpg")
-    fmt.Println(page.Title())
+	page.Navigate("https://www.google.com/")
+	page.Screenshot("./screenshot.jpg")
+	fmt.Println(page.Title())
 }
